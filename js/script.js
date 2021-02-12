5f2139aadd6be6b60a2845c1d80314d1a5d204aa
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-02-12 08:34:26 -0800

    trying to extract data from form; experimenting with firebase

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// FIXME
 function checkSubmitInfo() {
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
@@ -7,3 +8,22 @@ function checkSubmitInfo() {
 
 const button = document.getElementById('btnId');
 button.addEventListener('click', checkSubmitInfo());
+
+// Experimenting with Firebase
+
+// FIXME
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+var config = {
+apiKey: "apiKey",
+authDomain: "projectId.firebaseapp.com",
+// For databases not in the us-central1 location, databaseURL will be of the
+// form https://[databaseName].[region].firebasedatabase.app.
+// For example, https://your-database-123.europe-west1.firebasedatabase.app
+databaseURL: "https://databaseName.firebaseio.com",
+storageBucket: "bucket.appspot.com"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
