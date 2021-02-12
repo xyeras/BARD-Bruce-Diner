1ae92caec86312689002804f9d8b791cc6fb9c1c
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-02-12 08:15:06 -0800

    added media queries to resize text and size should it reach a certain screen size

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-function submitInfo {
+function checkSubmitInfo() {
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
-    const email = document.getElementById("email");
+    const email = document.getElementById("email").value;
     console.log(firstName + ' ' + lastName + ' - ' + email);
 }
 
-const button = document.getElementById('btnID');
-button.addEventListener('click', submitInfo());
+const button = document.getElementById('btnId');
+button.addEventListener('click', checkSubmitInfo());
