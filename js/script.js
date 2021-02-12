340f35d10fbfdf686336421d043dcf752104674e
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-02-12 12:49:01 -0800

    forgot to save file before git add; committing the newest file

--- js/script.js
+++ js/script.js
@@ -11,16 +11,16 @@ function handleSubmit() {
 // FIXME
 // Set the configuration for your app
 // TODO: Replace with your project's config object
-var config = {
-apiKey: "apiKey",
-authDomain: "projectId.firebaseapp.com",
-// For databases not in the us-central1 location, databaseURL will be of the
-// form https://[databaseName].[region].firebasedatabase.app.
-// For example, https://your-database-123.europe-west1.firebasedatabase.app
-databaseURL: "https://databaseName.firebaseio.com",
-storageBucket: "bucket.appspot.com"
-};
-firebase.initializeApp(config);
+// var config = {
+// apiKey: "apiKey",
+// authDomain: "projectId.firebaseapp.com",
+// // For databases not in the us-central1 location, databaseURL will be of the
+// // form https://g[databaseName].[region].firebasedatabase.app.
+// // For example, https://your-database-123.europe-west1.firebasedatabase.app
+// databaseURL: "https://databaseName.firebaseio.com",
+// storageBucket: "bucket.appspot.com"
+// };
+// firebase.initializeApp(config);
 
-// Get a reference to the database service
-var database = firebase.database();
+// // Get a reference to the database service
+// var database = firebase.database();
